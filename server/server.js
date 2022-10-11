73c3ac3269ac95fe36cca1595ba3071cbe91d1e8
Margarita Marmaridou <margmar@protonmail.com>
2022-10-11 10:11:14 +0300

    server file structure - connection to database

---
+++ server/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv"
+import cors from "cors"
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+const uri = process.env.MONGODB_URI;
+
+app.use(express.json());
+app.use(cors());
+
+//Connection to Database
+mongoose
+    .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("Successfully connected to database")
+    })
+    .catch(err => {
+        console.log("Database connection failed. Exiting now.");
+        console.error(err);
+    });
+
+app.listen(port, () => {
+    console.log(`Server is up at port ${port}`);
+});
