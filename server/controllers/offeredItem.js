99aef319dd0cb0ef780cc3fdbaa6cbe776062978
Margarita Marmaridou <margmar@protonmail.com>
2022-10-13 11:34:06 +0300

    filtered offeredItems & filteres requestedItems searches refactored

--- server/controllers/offeredItem.js
+++ server/controllers/offeredItem.js
@@ -2,68 +2,67 @@ import mongoose from "mongoose";
 import offeredItemModel from "../models/offeredItemModel.js";
 
 //Get all offered items
-export const getOfferedItems = async(req, res) => {
-    try {
-        const offeredItems = await offeredItemModel.find();
-        res.status(201).json({data: offeredItems});
-    }
-    catch(err) {
-        res.status(404).json({message: err.message});
-    }
+export const getOfferedItems = async (req, res) => {
+  try {
+    const offeredItems = await offeredItemModel.find();
+    res.status(201).json({ data: offeredItems });
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
 };
 
 //Get only those offered items that satisfy the search queries
-export const getOfferedItemsBySearch = async(req, res) => {
-    try {
-        const filtersArr = []
-        for (const filterKey of ['name', 'city', 'category']) {
-            if (req.query.filterKey) {
-                const thisFilter = {};
-                thisFilter[filterKey] = req.query.filterKey;
-                filtersArr.push(thisFilter);
-                console.log(filtersArr);
-            };
-        };
-        const filteredOfferedItems = await offeredItemModel.find(req.query);
-        res.status(201).json({data: filteredOfferedItems});
+export const getOfferedItemsBySearch = async (req, res) => {
+  try {
+    const filtersArr = [];
+    for (const filterKey of ["name", "city", "category"]) {
+      if (req.query[filterKey]) {
+        const thisFilter = {};
+        thisFilter[filterKey] = req.query[filterKey];
+        filtersArr.push(thisFilter);
+      }
     }
-    catch(err) {
-        res.status(404).json({message: err.message});
-    }
-}
+    const filteredOfferedItems = await offeredItemModel.aggregate([
+      {
+        $match: {
+          $and: filtersArr,
+        },
+      },
+    ]);
+    res.status(201).json({ data: filteredOfferedItems });
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
 
 //Get specific offered item by id
-export const getOfferedItem = async(req, res) => {
-    const { id } = req.params;
-    try {
-        const offeredItem = await offeredItemModel.find({_id: id})
-        res.status(201).json({data: offeredItem});
-    }
-    catch(err) {
-        res.status(404).json({message: err.message});
-    }
-}
+export const getOfferedItem = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const offeredItem = await offeredItemModel.find({ _id: id });
+    res.status(201).json({ data: offeredItem });
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
 
 //Create new offered item
-export const createOfferedItem = async(req, res) => {
-    const newOfferedItem = new offeredItemModel(req.body);
-    try {
-        await newOfferedItem.save();
-        res.status(201).send("Offered item created successfully");
-    }
-    catch(err) {
-        res.status(400).json({message: err.message});
-    }
+export const createOfferedItem = async (req, res) => {
+  const newOfferedItem = new offeredItemModel(req.body);
+  try {
+    await newOfferedItem.save();
+    res.status(201).send("Offered item created successfully");
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 };
 
 //Delete offered item
 export const deleteOfferedItem = async (req, res) => {
-    const { id } = req.params;
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-        return (
-            res.status(404).json({message: `No offered item with id: ${id}`})
-        )
-    };
-    await offeredItemModel.findByIdAndRemove({_id: id});
-    res.status(201).json({message: "Offered item deleted successfully."});
-}
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ message: `No offered item with id: ${id}` });
+  }
+  await offeredItemModel.findByIdAndRemove({ _id: id });
+  res.status(201).json({ message: "Offered item deleted successfully." });
+};
--- server/controllers/requestedItem.js
+++ server/controllers/requestedItem.js
@@ -2,68 +2,69 @@ import mongoose from "mongoose";
 import requestedItemModel from "../models/requestedItemModel.js";
 
 //Get all requested items
-export const getRequestedItems = async(req, res) => {
-    try {
-        const requestedItems = await requestedItemModel.find();
-        res.status(201).json({data: requestedItems});
-    }
-    catch(err) {
-        res.status(404).json({message: err.message});
-    }
+export const getRequestedItems = async (req, res) => {
+  try {
+    const requestedItems = await requestedItemModel.find();
+    res.status(201).json({ data: requestedItems });
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
 };
 
 //Get only those requested items that satisfy the search queries
-export const getRequestedItemsBySearch = async(req, res) => {
-    try {
-        const filtersArr = []
-        for (const filterKey of ['name', 'city', 'category']) {
-            if (req.query.filterKey) {
-                const thisFilter = {};
-                thisFilter[filterKey] = req.query.filterKey;
-                filtersArr.push(thisFilter);
-                console.log(filtersArr);
-            };
-        };
-        const filteredRequestedItems = await requestedItemModel.find(req.query);
-        res.status(201).json({data: filteredRequestedItems});
+export const getRequestedItemsBySearch = async (req, res) => {
+  try {
+    const filtersArr = [];
+    for (const filterKey of ["name", "city", "category"]) {
+      if (req.query[filterKey]) {
+        const thisFilter = {};
+        thisFilter[filterKey] = req.query[filterKey];
+        filtersArr.push(thisFilter);
+      }
     }
-    catch(err) {
-        res.status(404).json({message: err.message});
-    }
-}
+    const filteredRequestedItems = await requestedItemModel.aggregate([
+      {
+        $match: {
+          $and: filtersArr,
+        },
+      },
+    ]);
+    res.status(201).json({ data: filteredRequestedItems });
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
 
 //Get specific requested item by id
-export const getRequestedItem = async(req, res) => {
-    const { id } = req.params;
-    try {
-        const requestedItem = await requestedItemModel.find({_id: id})
-        res.status(201).json({data: requestedItem});
-    }
-    catch(err) {
-        res.status(404).json({message: err.message});
-    }
-}
+export const getRequestedItem = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const requestedItem = await requestedItemModel.find({ _id: id });
+    res.status(201).json({ data: requestedItem });
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
 
 //Create new requested item
-export const createRequestedItem = async(req, res) => {
-    const newRequestedItem = new requestedItemModel(req.body);
-    try {
-        await newRequestedItem.save();
-        res.status(201).send("requested item created successfully");
-    }
-    catch(err) {
-        res.status(400).json({message: err.message});
-    }
+export const createRequestedItem = async (req, res) => {
+  const newRequestedItem = new requestedItemModel(req.body);
+  try {
+    await newRequestedItem.save();
+    res.status(201).send("requested item created successfully");
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 };
 
 //Delete requested item
 export const deleteRequestedItem = async (req, res) => {
-    const { id } = req.params;
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-        return (
-            res.status(404).json({message: `No requested item with id: ${id}`})
-        )
-    };
-    await requestedItemModel.findByIdAndRemove({_id: id});
-    res.status(201).json({message: "requested item deleted successfully."});
-}
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(404)
+      .json({ message: `No requested item with id: ${id}` });
+  }
+  await requestedItemModel.findByIdAndRemove({ _id: id });
+  res.status(201).json({ message: "requested item deleted successfully." });
+};
