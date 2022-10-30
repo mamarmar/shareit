4d33e32a834fd681a6976f78258f99975eeeeb07
Margarita Marmaridou <margmar@protonmail.com>
2022-10-30 11:57:15 +0200

    use history instead of navigate

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,18 +1,19 @@
 import React from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useHistory } from "react-router-dom";
 
 export const AuthContext = React.createContext();
 
 function Auth({ children }) {
     const navigate = useNavigate();
+    const history = useHistory();
 
     const handleLogin = async(data) => {
         try {
             const res = await axios.post(`${process.env.REACT_APP_SERVER_URL}/user/login`, data);
             const token = res.data.user.token;
             localStorage.setItem('shareItToken', token);
-            navigate("/requested/new");
+            history.push("/requested/new");
             window.location.reload();
         }catch(err) {
             console.log(err);
